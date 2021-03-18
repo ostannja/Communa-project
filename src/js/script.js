7e5b5b84c026e7023a6242fdbf6bbd8fa55e20ec
Tetiana <ostannja@gmail.com>
2021-03-18 15:25:51 +0200

    modal registration / authorization

--- src/js/script.js
+++ src/js/script.js
@@ -1,259 +1,57 @@
-function testWebP(callback) {
+// "use strict";
+// Get the button that opens the modal
+var btn = document.querySelectorAll(".popup-button");
 
+// All page modals
+var modals = document.querySelectorAll(".popup");
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("closed");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (e) {
+    e.preventDefault();
+    popup = document.querySelector(e.target.getAttribute("href"));
+    popup.style.display = "block";
+    document.getElementById("body").style.overflow = "hidden";
+  };
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+  spans[i].onclick = function () {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+      document.getElementById("body").style.overflow = "scroll";
+    }
+  };
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target.classList.contains("popup")) {
+    for (var index in modals) {
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
+    }
+  }
+};
+
+function testWebP(callback) {
   var webP = new Image();
   webP.onload = webP.onerror = function () {
     callback(webP.height == 2);
   };
-  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
-
   if (support == true) {
-    document.querySelector('body').classList.add('webp');
+    document.querySelector("body").classList.add("webp");
   } else {
-    document.querySelector('body').classList.add('no-webp');
+    document.querySelector("body").classList.add("no-webp");
   }
 });
-
-"use strict";
-
-
-
-$(document).ready(function () {
-
-$('a[href*="#news"]').on('click', function (e) {
-    e.preventDefault();
-   
-    $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 2000, 'linear');
-  });
-})
-
-$('a[href*="#about"]').on('click', function (e) {
-  e.preventDefault();
- 
-  $('html, body').animate({
-    scrollTop: $($(this).attr('href')).offset().top
-  }, 1500, 'linear');
-});
-
-$('a[href*="#projects"]').on('click', function (e) {
-  e.preventDefault();
- 
-  $('html, body').animate({
-    scrollTop: $($(this).attr('href')).offset().top
-  }, 2500, 'linear');
-});
-
-$(document).ready(function () {
-
-  $('a[href*="#contact"]').on('click', function (e) {
-      e.preventDefault();
-     
-      $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-      }, 3000, 'linear');
-    });
-  })
-
-
-$(document).ready(function () {
-
-  $(".slider").slick({
-    dots: true,
-    slidesToShow: 3,
-    speed: 1000,
-    autoplaySpeed: 4000,
-    autoplay: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-  });
-});
-
-let map, coords, styles, marker, info, content;
-
-coords = { lat: 40.67, lng: -73.904 };
-styles = [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dadada"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#c9c9c9"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  }
-]
-
-content = "You`re here";
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: coords,
-    zoom: 12,
-    styles,
-  });
-
-  marker = new google.maps.Marker({
-    position: coords,
-    map: map,
-    icon: "img/map_marker.png",
-  });
-
-  info = new google.maps.InfoWindow({
-    content,
-  });
-
-  marker.addListener("click", () => info.open(map, marker));
-}
